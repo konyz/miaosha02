b92c78686cea6dd889dd6e588f7464cf8cc27e7b
eclipse_name <eclipse_user@konyz.com>
2019-09-16 09:09:39 +0800

    edit on branch hot_fix

---
+++ src/main/java/com/imooc/miaosha/TestMiaoShao.java
@@ -0,0 +1,17 @@
+package com.imooc.miaosha;
+
+import org.springframework.core.annotation.SynthesizedAnnotation;
+
+/**
+* @author 作者
+* @version 创建时间：2019年9月16日 上午9:05:02
+* 类说明
+*/
+public class TestMiaoShao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("测试git分支合并");
+	}
+
+}
